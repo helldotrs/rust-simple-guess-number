679068e8de4739294ec34d736e1f84d2d94ef38d
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-04 03:15:03 +0100

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,4 +1,13 @@
 import random
 
-num_random  = random.randrange(1,100)
+num_random  = 0
+num_input   = 0
+score       = 0
 
+while num_random != num_input {
+    if num_random == 0 {
+        num_random = random.randrange(1,100)
+    }
+}
+
+print(num_random)
