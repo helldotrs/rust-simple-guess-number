2a1e74a4ad1c4d6748d87064be4364a22ead5b47
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-04 03:25:19 +0100

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -8,6 +8,21 @@ while num_random != num_input {
     if num_random == 0 {
         num_random = random.randrange(1,100)
     }
+
+    #no error handeling
+    
+    if 1 <= num_input <= 100 {
+        if num_input < num_random {
+            print("guess a higher number!")
+        }
+        elif num_input > num_random {
+            print("guess a smaller number!")
+        }
+    }
+
+    num_input = input("guess a number 1-100: ")
+
+
 }
 
 print(num_random)
