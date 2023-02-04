29de8fdd72042e42054f2d0d63d0da1dee91fa29
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2023-02-04 04:18:58 +0100

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,28 +1,37 @@
+# prototype # by https://github.com/hellmak
 import random
 
-num_random  = 0
+num_max     = 100
+num_random  = num_max + 1
 num_input   = 0
 score       = 0
 
-while num_random != num_input {
-    if num_random == 0 {
-        num_random = random.randrange(1,100)
-    }
+print("Number input only or you will break the game.")
 
-    #no error handeling
+while num_random != num_input:
+    if num_random == num_max + 1:
+        num_random = random.randrange(1,num_max)
     
-    if 1 <= num_input <= 100 {
-        if num_input < num_random {
+    #cheats
+    if num_input == 1337:
+        print(f"cheat {num_random}")
+    if num_input == 1338:
+        score = 1
+        break
+    
+    if 1 <= num_input <= num_max:
+        if num_input < num_random:
             print("guess a higher number!")
-        }
-        elif num_input > num_random {
+        elif num_input > num_random:
             print("guess a smaller number!")
-        }
-    }
+        
+    
 
-    num_input = input("guess a number 1-100: ")
+    num_input = int(input(f"Guess a number 1-{num_max}: "))
+    
+    score += 1
 
-
-}
-
-print(num_random)
+if score == 1:
+    print("First guess, wow!")
+else:
+    print(f"You guessed the right number in {score} guesses!")
